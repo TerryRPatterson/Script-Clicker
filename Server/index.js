d7a29bd0a2d6ca26b333a9d0531c84698bb06a41
Terry Patterson <10890854+TerryRPatterson@users.noreply.github.com>
2018-04-30 15:04:56 -0400

    Fixed spacing

--- Server/index.js
+++ Server/index.js
@@ -2,15 +2,10 @@
 
 console.log("Server Starting");
 
-const {promisify} = require("util");
 const express = require("express");
 const app = express();
-const publicDirPath = "/home/terryp/rpggame/build";
-
-
-
+const publicDirPath = "/home/terryp/rpggame/build"; // need to set up env
 
 app.use("/",express.static(publicDirPath));
-
 
 app.listen(3000, () => console.log("Server is now listening."));
