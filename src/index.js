78a8c74bb0cdc93b75e78d45faff1822f60536f1
Prathyusha Mattam <prathyusha.mattam@gmail.com>
2018-04-30 15:32:09 -0400

    added store in index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
 import "./index.css";
 import Routes from "./Routes";
-// import App from "./App";
-import registerServiceWorker from "./registerServiceWorker";
+import reducer from "./reducers/index"
 
-ReactDOM.render(<Routes />, document.getElementById("root"));
-registerServiceWorker();
+const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+window.store = store;
+
+let reactAppReduxStore = 
+  <Provider store={store}>
+    <Routes />
+  </Provider>
+
+ReactDOM.render( reactAppReduxStore, document.getElementById('root'));
