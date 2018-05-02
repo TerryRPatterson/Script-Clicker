075fd6a73d510c14ac8db8af75c61bb6f89cbb7a
Ben Gamber <ben@stickfishstudio.com>
2018-05-02 15:52:18 -0400

    Organize user management functions into new file

--- Server/index.js
+++ Server/index.js
@@ -1,53 +1,15 @@
 #! /usr/bin/env node
 require('dotenv-safe').config();
 const express = require("express");
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
-const { findUserByEmail } = require('./database');
+const {
+  userLogin,
+  userRegister,
+  checkToken,
+  createToken
+} = require('./users');
 
 const app = express();
 let Router = express.Router;
-
-let checkToken = async (req, res, next) => {
-  let { authorization: token } = req.headers;
-  let payload;
-  try {
-    payload = jwt.verify(token, process.env.SIGNATURE);
-  } catch(err) {
-    console.log(err);
-  }
-
-  if (payload) {
-    req.jwt = payload;
-    next();
-  } else {
-    res.send(401);
-  }
-};
-
-let createToken = user =>
-  jwt.sign(
-    { userId: user.id },
-    process.env.SIGNATURE,
-    { expiresIn: '7d' }
-  );
-
-let userLogin = async (req, res) => {
-  let { email, password } = req.body;
-  let user = await findUserByEmail(email);
-
-  let isValid = await bcrypt.compare(password, user.pass);
-  if (isValid) {
-    let token = createToken(user);
-    res.send(token);
-  } else {
-    res.send(401, "Missing/Incorrect Password");
-  }
-};
-
-let userRegister = (req, res) => {
-  res.send("You went to Register!");
-};
 
 let tokenApi = new Router();
 tokenApi.post("/login", userLogin);
@@ -63,6 +25,6 @@ app.use(express.json());
 app.use(express.urlencoded());
 app.use("/auth", tokenApi);
 app.use("/api", api);
-app.use("/",express.static(process.env.PUBLICDIR));
+app.use("/", express.static(process.env.PUBLICDIR));
 
 app.listen(process.env.PORT || 3000, () => console.log("Server is now listening."));
